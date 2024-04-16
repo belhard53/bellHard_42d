071a1221c64b28c61283d5ae330eec03c2092aa6
bokus <bokus@tut.by>
2024-04-16 14:46:53 +0300

    add lesson3

---
+++ lesson3/example/1.py
@@ -0,0 +1,81 @@
+# name = "Dima"
+
+# a, b, c = 1, 2.1, 3
+# # a = b = c = 123
+# a, b = b, a
+# print(a, b, c)
+
+# us = "Ivanov"
+# print(us)
+
+# userName = "Vasia"
+# user_name = "Dima"
+# API_KEY = "d;slkds;ldks;dlks;ldk"
+# _ = 1
+
+# ok = True
+# ok = False
+
+# a = None # Пусто - отдельный тип данных для определения пустой переменной
+
+# print(type(a), type(user_name), type(ok), type(b), type(type))
+
+
+# price = 100
+# price = price + 2 / 3 * 4 - 12
+# price *= 1.3
+# a = 15
+# print(a % 4) # остаток
+# print(a // 7) 
+# print(a ** 2) # степень
+
+# a = 100_000_000_000
+# b = "-23_423"
+# print(a, b)
+
+
+# print(2**2**3)
+# print(36**0.5)
+# print(36 ** (1/2))
+# print(max(2,4,5)) # min, abs
+# print(round(2.234323, 2))
+# print(int(2.234323))
+# a = int("121")
+# b = float("111.123")
+# print(type(a), type(b))
+# print(a, b)
+# a = "122"
+# print(int(float(a)))
+
+# name = input('Как ваше имя :')
+# age = int(input("Сколько вам лет :"))
+# # age = int(age)
+# print("Вы родились в -", 2024 - age)
+
+# a = 12121212
+# a = 10 ** 1000
+# print(a)
+# a: int = 2
+# print("hello", 'world', 1, sep="", end='')
+# print(3, 5, 4, "hello", sep="-|-", end=" finish! ")
+# print(5, 4, 3)
+
+# a = "FFFF"
+# print(int(a, 16))
+# a = 126
+# print(oct(a))
+# print(bin(225)) 
+
+
+# print("путь к пррограмме - c:\net_work")
+# print(r"путь к пррограмме - c:\net_work")
+# print(r"путь к пррограмме - c:\\net_work")
+
+a = 1
+b = a
+b = 100
+print(a, b)
+print(a is b)
+print(id(a), id(b))
+
+
